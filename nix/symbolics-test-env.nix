d0b596927ae799caa21628c4d3c6757f77e8e6fe
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-26 16:10:23 -0400

    ci: include SWI-Prolog in focused Symbolics test env

--- nix/symbolics-test-env.nix
+++ nix/symbolics-test-env.nix
@@ -1,5 +1,11 @@
 { pkgs }:
-import ./python-env.nix {
-  inherit pkgs;
-  focused = true;
+let
+  python = import ./python-env.nix {
+    inherit pkgs;
+    focused = true;
+  };
+in
+pkgs.buildEnv {
+  name = "a0-symbolics-focused-test-env";
+  paths = [ python pkgs.swi-prolog ];
 }
