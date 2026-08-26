13001522d60a92d1d0e1f2ec0017d3683402823a
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-26 06:10:21 -0400

    Use production pins in focused Symbolics environment

--- nix/symbolics-test-env.nix
+++ nix/symbolics-test-env.nix
@@ -1,16 +1,5 @@
 { pkgs }:
-let
-  ps = pkgs.python312Packages;
-  packages = with ps; [
-    flask
-    litellm
-    markdown
-    pydantic
-    python-dotenv
-    pytest
-    pytest-asyncio
-    pytest-mock
-    tiktoken
-  ];
-in
-pkgs.python312.withPackages (_: packages)
+import ./python-env.nix {
+  inherit pkgs;
+  focused = true;
+}
